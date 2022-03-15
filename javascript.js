2caf38d9e7f64c6fca1b4f4e769ab6c155b55626
Abdullah Aldžić <94372357+aldzic4@users.noreply.github.com>
2022-03-16 00:25:28 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function funkcija(){
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+}
+}
+
+function funkcija2(){
+    var icon = document.getElementById("icon2");
+    icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+}
+
+}
+
+
